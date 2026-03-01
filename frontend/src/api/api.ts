cf91c2e139acef2e1610c89fff7bc6d2c7e52b0f
Akash Ajay Malvi <akashmalvi@gmail.com>
2026-03-01 15:38:48 +0530

    update backend url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: "https://ai-resume-2-tcdu.onrender.com", // backend URL
   withCredentials: true,
 });
 
